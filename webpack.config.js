b4c3dcd9f569e40bbda1dc8d65c3c4cf59baabb2
mixassio <mixail.bondarev@gmail.com>
2020-06-13 18:31:57 +0300

    Finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ const jsLoaders = () => {
 		{
 			loader: "babel-loader",
 			options: {
-				presets: ['@babel/preset-env']
+				presets: ['@babel/preset-env'],
+				plugins: ['@babel/plugin-proposal-class-properties']
 			}
 		}
 	]
@@ -34,7 +35,7 @@ module.exports = {
 		extensions: ['.js'],
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
-			'@core': path.resolve(__dirname, 'core')
+			'@core': path.resolve(__dirname, 'src/core')
 		}
 	},
 	devtool: isDev ? 'source-map' : false,
