e058d78f896c86a841dbcb5d89180e5082d836a1
mixassio <mixail.bondarev@gmail.com>
2020-06-28 22:53:30 +0300

    tests

---
+++ tests/createStore.test.js
@@ -0,0 +1,67 @@
+import { createStore } from '../src/core/createStore'
+
+const initialState = { count: 0 }
+const reducer = (state = initialState, action) => {
+	if (action.type === 'ADD') {
+		return { ...state, count: state.count + 1}
+	}
+	return state;
+}
+describe('createStore:', () => {
+	let store;
+	let handler
+	beforeEach(() => {
+		store = createStore(reducer, initialState)
+		handler = jest.fn()
+	})
+	test('should return store object', () => {
+		expect(store).toBeDefined()
+		expect(store.dispatch).toBeDefined()
+		expect(store.subscribe).toBeDefined()
+		expect(store.getState).toBeDefined()
+	})
+
+	test('should return object as a state', () => {
+		expect(store.getState()).toBeInstanceOf(Object)
+	})
+
+	test('should return default state', () => {
+		expect(store.getState()).toEqual(initialState)
+	})
+
+	test('should change state if action exist', () => {
+		store.dispatch({ type: 'ADD' })
+		expect(store.getState().count).toBe(1)
+	})
+
+	test('should NOT change state if action not exist', () => {
+		store.dispatch({ type: 'LALALA' })
+		expect(store.getState().count).toBe(0)
+	})
+
+	test('should called subscriber function', () => {
+		store.subscribe(handler)
+		store.dispatch({ type: 'ADD' })
+		expect(handler).toHaveBeenCalled()
+		expect(handler).toHaveBeenCalledWith(store.getState())
+	})
+
+	test('should not called subscriber function if unsubscribe', () => {
+		const sub = store.subscribe(handler)
+		sub.unsubscribe()
+		store.dispatch({ type: 'ADD' })
+		expect(handler).not.toHaveBeenCalled()
+	})
+
+	test('should dispatch in async way', () => {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				store.dispatch({ type: 'ADD' })
+			}, 500)
+			setTimeout(() => {
+				expect(store.getState().count).toBe(1)
+				resolve()
+			}, 1000)
+		})
+	})
+})
